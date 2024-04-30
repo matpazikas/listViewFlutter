4bda367a5d2709c385f552a6c8eb50eabdcc5c55
matpazikas <matheuspazikas@gmail.com>
2024-04-30 19:53:07 -0300

    cp2

--- lib/contato.dart
+++ lib/contato.dart
@@ -1,12 +1,19 @@
+
+import 'package:flutter/material.dart';
+
 class Contato {
   String nome;
   String email;
-  Contato(this.nome, this.email);
+  IconData ?icone;
+  Color ?cor;
+  String imagem;
+  Contato(this.nome, this.email, this.icone, this.cor, this.imagem);
 }
 
 List<Contato> contatos = [
-  Contato("Matheus", "mat@pikas.com"),
-  Contato("Marcelo", "ma@citrus.com"),
-  Contato("Miguel", "mano@miiii.com"),
-  Contato("Lima", "lima@mei.com"),
+  Contato("Matheus", "pazis@picapau.com", Icons.favorite_border, Colors.black, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnKKKA56zDkCnRvEP41qbyQHfwG3OQJrnzWd2g_qte9Q&s'),
+  Contato("Marcelo", "marcelo@citrus.com", Icons.favorite_border, Colors.black, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnZcUWYYr6a2LizVnA5Fb7xfOQm8X4njpuMxz0B3fa7A&s'),
+  Contato("Miguel", "spyro@palmeiras.com", Icons.favorite_border, Colors.black, 'https://pbs.twimg.com/profile_images/1759613268665016348/3s9UW9mw_400x400.jpg'),
+  Contato("Lima", "lima@santos.com", Icons.favorite_border, Colors.black, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIKNrc-ul6lmaTzw8HgtnR0P0bs9fbxYecfRVYGXtDWQ&s'),
+  Contato("Itália", "vichenzo@italy.com", Icons.favorite_border, Colors.black, 'https://s5.static.brasilescola.uol.com.br/be/2021/12/bandeira-da-italia.jpg'),
 ];
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:listview/contato.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -19,25 +24,54 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ListaPage extends StatelessWidget {
+class ListaPage extends StatefulWidget {
   const ListaPage({super.key});
+
+  @override
+  State<ListaPage> createState() => _ListaPageState();
+}
+
+
+
+class _ListaPageState extends State<ListaPage> {
+  int contador = 0; 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Listview builder", style: TextStyle(color: Colors.white),),
-        backgroundColor: Colors.blueAccent,),
+          title: Text(
+            "Contatos Favoritos $contador",
+            style: const TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.pink),
       body: ListView.builder(
-        itemCount: contatos.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile (
-            leading: CircleAvatar(child: Text(contatos[index].nome.substring(0, 1),)),
-            title: Text(contatos[index].nome),
-            subtitle: Text(contatos[index].email),
-          );
-        }),
+          itemCount: contatos.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(contatos[index].imagem),
+              ),
+              title: Text(contatos[index].nome),
+              subtitle: Text(contatos[index].email),
+              trailing: IconButton(
+                icon: Icon(contatos[index].icone, color: contatos[index].cor,),
+                onPressed: () {
+                  setState(() {
+                    if (contatos[index].icone == Icons.favorite_border) {
+                      contatos[index].icone = Icons.favorite;
+                      contatos[index].cor = Colors.red;
+                      contador++;
+                    } else {
+                      contatos[index].icone = Icons.favorite_border;
+                      contatos[index].cor = Colors.black;
+                      contador--;
+                    }
+                  });
+                },
+              ),
+            );
+          }),
     );
   }
 }
-
